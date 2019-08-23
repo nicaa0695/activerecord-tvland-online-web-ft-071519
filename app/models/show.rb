83e11147174f7b5c5bdc0bbb92d0bdd711deee6c
Veronica Lopez <nicaa0695@gmail.com>
2019-08-23 17:47:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -3,4 +3,9 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
   
+  def actors_list 
+    self.actors.collect do |actor|
+      actor.full_name 
+    end 
+  end 
 end
